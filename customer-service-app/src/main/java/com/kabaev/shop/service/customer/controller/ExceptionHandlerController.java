6e25f8f57326dc8dbeea69ec9261bb960a2df076
Timur Kabaev <timur_kabaev@epam.com>
2022-03-14 17:01:32 +0300

    feature_05: add isDeleted field to Product

---
+++ customer-service-app/src/main/java/com/kabaev/shop/service/customer/controller/ExceptionHandlerController.java
@@ -0,0 +1,40 @@
+package com.kabaev.shop.service.customer.controller;
+
+import com.kabaev.shop.service.customer.dto.ExceptionResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponseDto> handleUnexpectedExceptions(
+            HttpServletRequest request,
+            Exception e) {
+        var id = UUID.randomUUID().toString();
+        log.error("Unexpected error: [incidentId={}, url={}, method={}]",
+                id, request.getRequestURL(), request.getMethod(), e);
+        List<String> stackTrace = Arrays.stream(e.getStackTrace())
+                .map(Objects::toString)
+                .toList();
+        return new ResponseEntity<>(
+                new ExceptionResponseDto(
+                        "Unexpected error: please contact support regarding incident " + id,
+                        LocalDateTime.now(),
+                        stackTrace),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+}
--- customer-service-app/src/main/java/com/kabaev/shop/service/customer/feign/KeeperServiceClient.java
+++ customer-service-app/src/main/java/com/kabaev/shop/service/customer/feign/KeeperServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "keeperService", url = "http://localhost:8080/api/v1/products")
+@FeignClient(name = "keeperService", url = "${keeper.service.url}")
 public interface KeeperServiceClient {
 
     @GetMapping
